437295629fbf8ec133e342b67a871c2d4da29635
changlin li <changlinli@changlins-MacBook-Air.local>
2025-08-29 14:18:22 -0400

    lab2 demo

---
+++ L2/Ex7_if.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ex7_if {
+  public static void main(String[] args) {
+    // block of code to be executed
+    // if the condition is true
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter x:");
+    int x = input.nextInt();
+    System.out.print("Enter y:");
+    int y = input.nextInt();
+    if (x > y) 
+      System.out.println(x + " is greater than " + y);
+    System.out.println("This is not part of if");
+  }
+}
---
+++ L2/Ex8_else.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex8_else {
+  public static void main(String[] args) {
+    // block of code to be executed
+    // if the condition is true
+    Scanner input = new Scanner(System.in);
+
+    // if-else
+    System.out.print("Enter time:");
+    int time = input.nextInt();
+    if (time < 18) {
+      System.out.println("Good day.");
+    } else {
+      System.out.println("Good evening.");
+    }
+  }
+}
