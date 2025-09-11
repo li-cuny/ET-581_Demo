c4907132bb9787f664340310a6056e40862662e1
Changlin Li <changlin.li@qcc.cuny.edu>
2025-09-11 09:09:58 -0400

    add L4

---
+++ L4/Ex13_random.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Ex12_traverse_2d_array {
+  public static void main(String[] args) {
+    System.out.println(Math.random()); // return 0 to 1.0
+    System.out.println((int)(Math.random()*100)); // int 0 to 100
+    System.out.println(60 + (int)(Math.random()*40)); // 60 to 100
+  }
+}
---
+++ L4/Ex9_length.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Ex9_length {
+  public static void main(String[] args) {
+    int[] a = {1,2,3,4};
+    System.out.println(a.length);
+
+    int[] b = new int[10];
+    System.out.println(b.length);
+  }
+}
