c30e4142ab586b65696793656aee30f5a038bb3a
li-cuny <changlin.li@qcc.cuny.edu>
2025-09-11 15:12:29 +0000

    add array class

---
+++ L4/Ex14_array_class.java
@@ -0,0 +1,13 @@
+
+public class Ex14_array_class {
+  public static void main(String[] args) {
+    int[] nums = new int[3];
+    double[] doubles = new double[3];
+    String str = "this is a String";
+    String[] strings = new String[3];
+    System.out.println(nums.getClass().getName());
+    System.out.println(doubles.getClass().getName());
+    System.out.println(str.getClass().getName());
+    System.out.println(strings.getClass().getName());
+  }
+}
--- L4/Ex1_rand.java
+++ L4/Ex1_rand.java
@@ -1,9 +1,11 @@
-
-public class Ex13_random {
+import java.util.Scanner;
+import java.util.Arrays;
+public class Ex1_rand {
   public static void main(String[] args) {
     System.out.println(Math.random()); // return 0 to 1.0
     System.out.println((int)(Math.random()*100)); // int 0 to 100
     System.out.println(60 + (int)(Math.random()*40)); // 60 to 100
+    double d = 0.2;
    
   }
 }
