f056b4a6fbb63b9fc4e0dc66d0ca442db9859368
li-cuny <changlin.li@qcc.cuny.edu>
2025-09-15 17:25:31 +0000

    update

--- L5/Ex12_1D_array.java
+++ L5/Ex12_1D_array.java
@@ -1,22 +1,28 @@
 public class Ex12_1D_array {
   // array as parameter to function
   static void output(int[] a) {
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       System.out.print(a[i] + " ");
+    }
+
     System.out.println();
   }
 
   // update array in function
   static void update(int[] a) {
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       a[i] *= 10;
+    }
+
   }
 
   // create a new array obj with given size
   static int[] return_array(int size) {
     int[] a = new int[size];
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       a[i] = i + 1;
+    }
+
     return a;
   }
 
