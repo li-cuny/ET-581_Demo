8aca4afc6220cf1837631decc8032ef9351eee09
li-cuny <changlin.li@qcc.cuny.edu>
2025-09-16 00:06:52 +0000

    format

--- L4/Ex03_for.java
+++ L4/Ex03_for.java
@@ -3,18 +3,25 @@ public class Ex03_for {
     // Array
     // type [] var = {values};
     int[] myNum = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-    for (int i = 0; i < myNum.length; i++)
+    for (int i = 0; i < myNum.length; i++) {
       System.out.println(i + " " + myNum[i]);
+    }
+
     // System.exit(1);
 
     String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-    for (int i = 0; i < cars.length; i++)
+    for (int i = 0; i < cars.length; i++) {
       System.out.println(i + " " + cars[i]);
+    }
 
     // could update array use indexed for loop
-    for (int i = 0; i < myNum.length; i++)
+    for (int i = 0; i < myNum.length; i++) {
       myNum[i] *= 10;
-    for (int i = 0; i < myNum.length; i++)
+    }
+
+    for (int i = 0; i < myNum.length; i++) {
       System.out.println(i + " " + myNum[i]);
+    }
+
   }
 }
