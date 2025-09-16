cbac29ca4984e062f6724d518a1c525f4875ddf8
li-cuny <changlin.li@qcc.cuny.edu>
2025-09-16 00:20:58 +0000

    format

--- L4/Ex06_shallow_copy.java
+++ L4/Ex06_shallow_copy.java
@@ -2,32 +2,44 @@
 public class Ex06_shallow_copy {
   public static void main(String[] args) {
     int[] a = { 1, 2, 3, 4 };
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       System.out.print(a[i] + " "); // 1 2 3 4
+    }
+
     System.out.println();
     int[] b = a; // shallow copy
-    for (int i = 0; i < b.length; i++)
+    for (int i = 0; i < b.length; i++) {
       System.out.print(b[i] + " "); // 1 2 3 4
+    }
+
     System.out.println();
     // System.exit(0);
 
     a[0] = 10;
 
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       System.out.print(a[i] + " "); // 10 2 3 4?
+    }
+
     System.out.println();
-    for (int i = 0; i < b.length; i++)
+    for (int i = 0; i < b.length; i++) {
       System.out.print(b[i] + " "); // 1 2 3 4?
+    }
+
     System.out.println();
     // System.exit(0);
 
     b[1] = 200;
 
-    for (int i = 0; i < a.length; i++)
+    for (int i = 0; i < a.length; i++) {
       System.out.print(a[i] + " "); // 10 2 3 4?
+    }
+
     System.out.println();
-    for (int i = 0; i < b.length; i++)
+    for (int i = 0; i < b.length; i++) {
       System.out.print(b[i] + " "); // 1 200 3 4?
+    }
+
     System.out.println();
     // System.exit(0);
 
